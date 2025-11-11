d93dc13a06a6c9a9472af57fd04a814e6681ba23
Abdallah Mahmoud Abou Hussein <abdallah2006ok@gmail.com>
2025-11-11 09:52:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,20 +10,6 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
-class Person{
-   Person(string n , int id){
-      name = n;
-      this.id = id; 
-   }
-
-   void display(){
-      cout<< Person.name << " "<<Person.id<<endl;
-      
-   }
-
-}
-
-
 
 
 
@@ -31,31 +17,12 @@ class Person{
 // ==================== Student Class Implementation ====================
 
 
-class Student{
-   Student(string n , int i, int yl , string m ){
-      super(name , id);
-      yearLevel = yl;
-      major =m;
-   }
-   void display(){
-      cout << Student.name<<" "<<Student.id<<" "<<Student.yearLevel<<" "<<Student.major<<endl;
-   }
-}
+
 
 
 
 // ==================== Instructor Class Implementation ====================
-class Instructor{
 
-   Instructor(string n , int i, string d , int ey){
-         super(name,id);
-         departement = d;
-         experienceYears = ey;
-   }
-   void display(){
-         cout << Instructor.name<<" "<<Instructor.id<<" "<<Instructor.departement<<" "<<Instructor.experienceYears<<endl;
-      }
-   }
 
 
 
@@ -65,23 +32,7 @@ class Instructor{
 
 // ==================== Course Class Implementation ====================
 
-class Course{
-   Course(string cc,string cn,int ms, Student* s , int cs){
-         courseCode = cc;
-         courseName = cn;
-         maxStudents = ms;
-         currentStudents = cs;
-   }
-   Student addStudent(const Student& s){
-      currentStudents++;
-      return currentStudents;
-   }
-void displayCourseInfo(){
-        cout <<Course.courseCode<< Course.name<<" "<<Course.maxStudents<<" "<<Course.students<<" "<<Course.currentStudents<<endl;
-      Student::display();
 
-   }
-}
 
 
 
