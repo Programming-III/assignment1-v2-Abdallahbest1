c909c6265c6420df7194a2cf9725c09c2b67a8d0
Abdallah Mahmoud Abou Hussein <abdallah2006ok@gmail.com>
2025-11-11 09:52:15 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,30 @@
-#ifndef PERSON_H
-#define PERSON_H
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
 
-#include <string>
+#include "Person.h"
 using namespace std;
+//#write Instructor class here
 
-//#write Person class here
- class  Person{
 
-private:
-  string name;
-  int id;
+class Instructor: public Person{
+
 public :
- void display();
-
+  string departement;
+  int experienceYears;
+  void display();
+  
+  Instructor(string n , int i, string d , int ey){
+         super(name,id);
+         departement = d;
+         experienceYears = ey;
+   }
+   void display(){
+         cout << Instructor.name<<" "<<Instructor.id<<" "<<Instructor.departement<<" "<<Instructor.experienceYears<<endl;
+      }
+   }
 };
+
+
 
 
 
