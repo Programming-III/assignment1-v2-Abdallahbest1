50ea324676ff079c7dab1f6e1d883dc53335690d
Abdallah Mahmoud Abou Hussein <abdallah2006ok@gmail.com>
2025-11-11 09:42:59 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -11,6 +11,10 @@ private:
   string name;
   int id;
 public :
+Person(string n , int id){
+      name = n;
+      this.id = id; 
+   }
  void display(){
       cout<< Person.name << " "<<Person.id<<endl;
       
