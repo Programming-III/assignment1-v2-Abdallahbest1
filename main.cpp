ca4069d8d661edbc7b2922e87622350111bfe97e
Abdallah Mahmoud Abou Hussein <abdallah2006ok@gmail.com>
2025-11-11 09:40:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,13 +32,31 @@ class Person{
 
 
 class Student{
-   Student(
-
+   Student(string n , int i, int yl , string m ){
+      super(name , id);
+      yearLevel = yl;
+      major =m;
+   }
+   void display(){
+      cout << Student.name<<" "<<Student.id<<" "<<Student.yearLevel<<" "<<Student.major<<endl;
+   }
 }
 
 
 
 // ==================== Instructor Class Implementation ====================
+class Instructor{
+
+   Instructor(string n , int i, string d , int ey){
+         super(name,id);
+         departement = d;
+         experienceYears = ey;
+   }
+   void display(){
+         cout << Instructor.name<<" "<<Instructor.id<<" "<<Instructor.departement<<" "<<Instructor.experienceYears<<endl;
+      }
+   }
+
 
 
 
@@ -47,7 +65,23 @@ class Student{
 
 // ==================== Course Class Implementation ====================
 
+class Course{
+   Course(string cc,string cn,int ms, Student* s , int cs){
+         courseCode = cc;
+         courseName = cn;
+         maxStudents = ms;
+         currentStudents = cs;
+   }
+   Student addStudent(const Student& s){
+      currentStudents++;
+      return currentStudents;
+   }
+void displayCourseInfo(){
+        cout <<Course.courseCode<< Course.name<<" "<<Course.maxStudents<<" "<<Course.students<<" "<<Course.currentStudents<<endl;
+      Student::display();
 
+   }
+}
 
 
 
